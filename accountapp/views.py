c94b3e2c48848c4b5bf7a99e7adc67141286ffaf
juungchull <chul6147@gmail.com>
2022-03-20 14:35:08 +0900

    screat

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
 # Create your views here.
 def hello_world(requests):
-    return HttpResponse
+    return HttpResponse('반갑습니다.')
